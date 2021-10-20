edf06964fe3f806e45d6cbfe133eec4128bcaa26
swapnil.g <swapnilg@zeta.tech>
2021-10-20 10:48:58 +0530

    Adding initial code

---
+++ paymentsdk/src/msthemis/java/com/example/swapnil/paymentsdk/MasterCardTest.kt
@@ -0,0 +1,12 @@
+package com.example.swapnil.paymentsdk
+
+import android.util.Log
+
+class MasterCardTest {
+
+    companion object {
+        fun print() {
+            Log.d("TAG", "This is the master themis service")
+        }
+    }
+}
